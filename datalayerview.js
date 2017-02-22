1b39a04e7e20a5f9638d9d80ae4b4b43687ac43f
jonnaro <jonnaro@gmail.com>
2017-02-22 20:27:57 +0800

    iterate through pageInfo object

--- datalayerview.js
+++ datalayerview.js
@@ -1,13 +1,33 @@
 function readDL(domContent) {
 
+  var dl = window.digitalData;
+
   console.group('digitalData - w3c Data Layer View');
 
   if (window.digitalData === undefined) {
     console.log('The digitalData object is not defined on this page.');
   }
   else {
-    console.log('pageInstanceID  : ' + window.digitalData.pageInstanceID);
-    console.log('siteID          : ' + window.digitalData.siteID);
+
+    //site-related
+    if (dl.siteID) { console.log('pageInstanceID : ' + dl.siteID); }
+
+    //page-related
+    if (dl.pageInstanceID) { console.log('pageInstanceID : ' + dl.pageInstanceID); }
+
+    //iterate through pageInfo object
+    if (dl.page.pageInfo) {
+      branch = dl.page.pageInfo;
+      for (var key in branch) {
+        if (branch.hasOwnProperty(key) && branch[key]) {
+          console.log('pageInfo.' + key + " : " + branch[key]);
+        }
+        else {
+          console.log('pageInfo.' + key + " : NOT DEFINED");
+        }
+      }
+    }
+
   }
 
   console.groupEnd();
